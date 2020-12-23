ebf564738864591243e86840ea919461975d77e6
Ting Chen <chenting_cn@foxmail.com>
2020-12-23 16:56:55 +0200

    Exercises 4.4

--- blog/tests/dummy.test.js
+++ blog/tests/dummy.test.js
@@ -1,5 +1,4 @@
 const listHelper = require('../utils/list_helper')
-
 test('dummy returns one', () => {
   const blogs = []
   const result = listHelper.dummy(blogs)
--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -1,6 +1,16 @@
 const dummy = (blogs) => {
   return 1
 }
+
+const totalLikes = (blogsList) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogsList.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
