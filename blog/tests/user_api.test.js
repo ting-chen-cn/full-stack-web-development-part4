155038414db2b68cc061d15efd6295e92969b3d0
Ting Chen <chenting_cn@foxmail.com>
2020-12-27 03:03:14 +0200

    Exercises 4.18

--- blog/tests/user_api.test.js
+++ blog/tests/user_api.test.js
@@ -4,7 +4,6 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const User = require('../models/user')
-const { expect } = require('@jest/globals')
 
 beforeEach(async () => {
   await User.deleteMany({})
