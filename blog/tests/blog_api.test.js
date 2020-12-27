181c5d026621a759acdd57f596ffbf8f2000e874
Ting Chen <chenting_cn@foxmail.com>
2020-12-27 03:25:19 +0200

    Exercises 4.19

--- blog/tests/blog_api.test.js
+++ blog/tests/blog_api.test.js
@@ -77,7 +77,6 @@ test('the unique identifier of blogs is named id', async () => {
   )
   await Promise.all(promiseArray)
   const idArray = promiseArray.map((r) => r.response.body.id)
-
   expect(idArray).toStrictEqual(blogsAtStart.map((r) => r.id))
 })
 
@@ -97,13 +96,9 @@ test('a blog can be deleted', async () => {
   const blogToDelete = blogsAtStart[0]
 
   await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
-
   const blogsAtEnd = await helper.blogsInDb()
-
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
-
   const titles = blogsAtEnd.map((r) => r.title)
-
   expect(titles).not.toContain(blogToDelete.title)
 })
 
